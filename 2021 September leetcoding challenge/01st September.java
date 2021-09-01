808c5f7c72b6e7f0ce1a8ae2a8be74c630b5bde7
Gautam Wadhwani <72446354+gautamsw5@users.noreply.github.com>
2021-09-01 12:57:50 +0530

    Update 01st September.java

--- 2021 September leetcoding challenge/01st September.java
+++ 2021 September leetcoding challenge/01st September.java
@@ -1,1 +1,28 @@
-i
+class Solution
+{
+    public int arrayNesting(int[] arr)
+    {
+        this.dp = new int[arr.length];
+        this.arr = arr;
+        int ans = 0;
+        for(int i = 0; i < arr.length; i++)
+            if(dp[i] == 0)
+            {
+                this.vis = new HashSet<>();
+                ans = Math.max(ans, dfs(i));
+            }
+        return ans;
+    }
+    private int dfs(int i)
+    {
+        if(vis.contains(i))
+            return 0;
+        vis.add(i);
+        if(dp[i] != 0)
+            return dp[i];
+        dp[i] = 1 + dfs(arr[i]);
+        return dp[i];
+    }
+    private int[] arr, dp;
+    private Set<Integer> vis;
+}
