e7e19918d26e1a314802d530051b78a453d7b9b4
Gautam Wadhwani <72446354+gautamsw5@users.noreply.github.com>
2021-09-07 12:48:32 +0530

    Create 07th September.java

---
+++ 2021 September leetcoding challenge/07th September.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode newHead = head, temp;
+        if(head != null) {
+            temp = head.next;
+            head.next = null;
+            head = temp;
+        }
+        while(head != null) {
+            temp = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = temp;
+        }
+        return newHead;
+    }
+}
