404e791c2cc02ab5a5c812bb3b0913c7a6d50c96
Gautam Wadhwani <72446354+gautamsw5@users.noreply.github.com>
2021-11-13 08:58:44 +0530

    Update 13th November.java

--- 2021 November leetcoding challenge/13th November.java
+++ 2021 November leetcoding challenge/13th November.java
@@ -1,21 +1,23 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        TreeSet<Integer>[] indices = new TreeSet[102];
+        TreeSet<Integer>[] indices = new TreeSet[102];   // Used treeset so that higher function can be used
         int[] ans = new int[temperatures.length];
         Arrays.fill(ans, Integer.MAX_VALUE);
         for(int i = 0; i < 102; i++) {
             indices[i] = new TreeSet<Integer>();
         }
+		
         for(int i = 0; i < temperatures.length; i++) {
-            indices[temperatures[i]].add(i);
+            indices[temperatures[i]].add(i);                   // Add every index in its corresponding temperature
         }
+		
         for(int i = 0; i < temperatures.length; i++) {
-            for(int j = temperatures[i]+1; j < 102; j++) {
+            for(int j = temperatures[i]+1; j < 102; j++) {     // Check for all values greater than temperature[i] whether they occur after i or not and get the minimum such index
                 Integer x = indices[j].higher(i);
                 if(x != null) ans[i] = Math.min(ans[i], x);
             }
-            if(ans[i] == Integer.MAX_VALUE) ans[i] = 0;
-            else ans[i] -= i;
+            if(ans[i] == Integer.MAX_VALUE) ans[i] = 0;  // Not found so 0
+            else ans[i] -= i;      // Number of days to wait
         }
         return ans;
     }
