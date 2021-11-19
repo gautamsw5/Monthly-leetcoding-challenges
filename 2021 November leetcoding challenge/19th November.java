8b26b23926fc3bff8d6af1ba543d58f7d52085d6
Gautam Wadhwani <72446354+gautamsw5@users.noreply.github.com>
2021-11-19 08:40:52 +0530

    Create 19th November.java

---
+++ 2021 November leetcoding challenge/19th November.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int dist = 0;
+        while(x > 0 || y > 0) {
+            dist += (x & 1)^(y & 1);
+            x = x >> 1;
+            y = y >> 1;
+        }
+        return dist;
+    }
+}
