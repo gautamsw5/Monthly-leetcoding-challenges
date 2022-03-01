2a09ef50623dbc82e222a1dacbcf2d8361e3b39f
Gautam Wadhwani <72446354+gautamsw5@users.noreply.github.com>
2022-03-01 09:28:06 +0530

    Update 01 Mar.py

--- 2022/March/01 Mar.py
+++ 2022/March/01 Mar.py
@@ -1,1 +1,6 @@
-import sys
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        arr = [0, 1]
+        for i in range(2, n+1):
+            arr.append(arr[i>>1] + (i&1))
+        return arr[:n+1]
