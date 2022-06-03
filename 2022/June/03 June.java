40840c9628c8ef1eb09dad9f6643f236d7dd89cf
Gautam Wadhwani <72446354+gautamsw5@users.noreply.github.com>
2022-06-03 21:48:51 +0530

    Create 03 June.java

---
+++ 2022/June/03 June.java
@@ -0,0 +1,25 @@
+class NumMatrix {
+    private int[][] arr;
+    public NumMatrix(int[][] matrix)
+    {
+        arr=matrix;
+    }
+    public int sumRegion(int row1, int col1, int row2, int col2)
+    {
+        int s=0;
+        for(int i=row1;i<=row2;i++)
+        {
+            for(int j=col1;j<=col2;j++)
+            {
+                s=s+arr[i][j];
+            }
+        }
+        return s;
+    }
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */
